5ebf297dcc7a22ee7698468df23ac3a9c14fd967
VesTnik1320 <zhurin.ivan33@outlook.com>
2026-02-28 20:19:28 +0300

    Update prime functions and tests

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,23 +2,54 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+    if (value <= 1)
+        return false;
+
+    uint64_t count = 0;
+    for (uint64_t i = 2; i < value; i++) {
+        if (value % i == 0) {
+            count++;
+        }
+    }
+
+    return count == 0;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    if (n == 0) return 0;
+
+    uint64_t count = 0;
+    uint64_t num = 1;
+
+    while (count < n) {
+        num++;
+        if (checkPrime(num)) {
+            count++;
+        }
+    }
+
+    return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+    uint64_t num = value + 1;
+
+    while (!checkPrime(num)) {
+        num++;
+    }
+
+    return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    uint64_t sum = 0;
+
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
 }
