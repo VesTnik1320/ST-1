1b339b87f1450647f25ca0918be93d2311b7004d
VesTnik1320 <zhurin.ivan33@outlook.com>
2026-06-01 11:05:42 +0300

    Refactor prime checking and related functions

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,29 +3,40 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-    if (value <= 1)
+    if (value < 2) {
         return false;
+    }
 
-    uint64_t count = 0;
-    for (uint64_t i = 2; i < value; i++) {
+    if (value == 2) {
+        return true;
+    }
+
+    if (value % 2 == 0) {
+        return false;
+    }
+
+    for (uint64_t i = 3; i <= value / i; i += 2) {
         if (value % i == 0) {
-            count++;
+            return false;
         }
     }
 
-    return count == 0;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) return 0;
+    if (n == 0) {
+        return 0;
+    }
 
     uint64_t count = 0;
     uint64_t num = 1;
 
     while (count < n) {
-        num++;
+        ++num;
+
         if (checkPrime(num)) {
-            count++;
+            ++count;
         }
     }
 
@@ -36,7 +47,7 @@ uint64_t nextPrime(uint64_t value) {
     uint64_t num = value + 1;
 
     while (!checkPrime(num)) {
-        num++;
+        ++num;
     }
 
     return num;
@@ -45,7 +56,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
 
-    for (uint64_t i = 2; i < hbound; i++) {
+    for (uint64_t i = 2; i < hbound; ++i) {
         if (checkPrime(i)) {
             sum += i;
         }
